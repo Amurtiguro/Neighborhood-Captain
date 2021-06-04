c2ef633c98c126661dc640817c620e9bb990507c
LK <dlarlsud@gmail.com>
2021-06-04 19:31:46 +0900

    del commit

--- spw/src/com/spw/deal/dao/DealDao.java
+++
@@ -1,9 +0,0 @@
-package com.spw.deal.dao;
-
-import com.spw.deal.vo.DealVO;
-
-public interface DealDao {
-
-	// 거래 신청. ===============================
-	public int dealInsert(DealVO dvo);
-}
--- spw/src/com/spw/deal/service/DealService.java
+++
@@ -1,9 +0,0 @@
-package com.spw.deal.service;
-
-import com.spw.deal.vo.DealVO;
-
-public interface DealService {
-	
-	// 거래 신청. ===============================
-	public int dealInsert(DealVO dvo);
-}
